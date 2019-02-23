3046622a640c6e1b488b4ec22fce1e6ba60b8cbf
Luiz Cezer <lccezinha@gmail.com>
2019-02-23 19:04:08 -0300

    Print result for first input list

--- lib/tax_calculator.rb
+++ lib/tax_calculator.rb
@@ -1,4 +1,16 @@
-require "tax_calculator/version"
+require_relative "tax_calculator/version"
+require_relative "tax_calculator/file_reader"
+require_relative "tax_calculator/calculator"
+require_relative "tax_calculator/printer"
 
 module TaxCalculator
+  def self.run
+    ['./input_1.md'].each do |file_path|
+      products = FileReader.new(file_path).list_products
+      calculated_result = Calculator.new(products).calculate
+      puts Printer.new(calculated_result).print
+    end
+  end
 end
+
+TaxCalculator.run
