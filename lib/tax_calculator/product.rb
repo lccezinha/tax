b201ea839626bebcdc99cf5c218d11e6ab9a75c3
Luiz Cezer <lccezinha@gmail.com>
2019-02-24 01:32:44 -0300

    Create TaxCalculator::Product class

---
+++ lib/tax_calculator/product.rb
@@ -0,0 +1,34 @@
+module TaxCalculator
+  class Product
+    def initialize(data)
+      @data = data
+    end
+
+    def name
+      data[:name]
+    end
+
+    def quantity
+      data[:quantity]
+    end
+
+    def price
+      data[:price]
+    end
+
+    def imported?
+      data[:name].include?('imported')
+    end
+
+    def category
+      return :books if data[:name].include?('book')
+      return :food if data[:name].include?('chocolate')
+      return :medical if data[:name].include?('pills')
+      :other
+    end
+
+    private
+
+    attr_reader :data
+  end
+end
---
+++ spec/lib/tax_calculator/product_spec.rb
@@ -0,0 +1,67 @@
+require_relative '../../../lib/tax_calculator/product'
+
+RSpec.describe TaxCalculator::Product do
+  subject(:product) { described_class.new(data) }
+
+  describe "attributes" do
+    let(:data) do
+      { quantity: 1, name: 'imported box of chocolates', price: 10 }
+    end
+
+    it { expect(product.name).to eq(data[:name]) }
+    it { expect(product.quantity).to eq(data[:quantity]) }
+    it { expect(product.price).to eq(data[:price]) }
+  end
+
+  describe ".imported?" do
+    context "when product name contain word imported" do
+      let(:data) do
+        { quantity: 1, name: 'imported box of chocolates', price: 10 }
+      end
+
+      it { expect(product.imported?).to be_truthy }
+    end
+
+    context "when product name does not contain word imported" do
+      let(:data) do
+        { quantity: 1, name: 'box of chocolates', price: 10 }
+      end
+
+      it { expect(product.imported?).to be_falsey }
+    end
+  end
+
+  describe ".category" do
+    context 'when product in a book' do
+      let(:data) do
+        { quantity: 1, name: 'imported books', price: 10 }
+      end
+
+      it { expect(product.category).to eq(:books) }
+    end
+
+    context 'when product in a food' do
+      let(:data) do
+        { quantity: 1, name: 'chocolates bar', price: 10 }
+      end
+
+      it { expect(product.category).to eq(:food) }
+    end
+
+    context 'when product in a medical' do
+      let(:data) do
+        { quantity: 1, name: 'some pills', price: 10 }
+      end
+
+      it { expect(product.category).to eq(:medical) }
+    end
+
+    context 'when product in a anything else' do
+      let(:data) do
+        { quantity: 1, name: 'foobar', price: 10 }
+      end
+
+      it { expect(product.category).to eq(:other) }
+    end
+  end
+end
